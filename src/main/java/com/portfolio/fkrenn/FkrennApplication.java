b2c95814f92b9919fbe4831930bd911714045d97
Fede Krenn <fedekrenn@gmail.com>
2022-07-22 22:14:01 -0300

    Se inicializa repo para back con springboot

---
+++ src/main/java/com/portfolio/fkrenn/FkrennApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.fkrenn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkrennApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkrennApplication.class, args);
+	}
+
+}
