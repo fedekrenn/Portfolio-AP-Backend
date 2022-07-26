5a1bef169ee4daf8d0e041db3014749801297b3f
Fede Krenn <fedekrenn@gmail.com>
2022-07-26 00:40:13 -0300

    Se termina la lógica de Login con JWT

--- src/main/java/com/portfolio/fkrenn/Controller/PersonaController.java
+++ src/main/java/com/portfolio/fkrenn/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.fkrenn.Entity.Persona;
 import com.portfolio.fkrenn.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,18 +25,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "Se creó correctamente la persona!";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "Se eliminó la persona correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
